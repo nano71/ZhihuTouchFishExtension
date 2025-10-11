0db5ed69d3f9c8791e9f9c7f0cbe7a53babcb0dd
nano71 <80870408+nano71@users.noreply.github.com>
2025-10-11 09:14:26 +0800

    优化细节

--- content.js
+++ content.js
@@ -1,10 +1,8 @@
 (() => {
-    console.log("[Zhihu DOM Modifier] content script loaded");
     document.querySelectorAll("header a")[0]?.remove()
     document.querySelector("header .SearchBar-askContainer")?.remove()
 
     function modifyDOM() {
-
         document.querySelectorAll(".Button").forEach(element => {
             const label = element.getAttribute("aria-label")
             if (label && label !== element.textContent)
@@ -12,33 +10,21 @@
         })
 
         document.querySelectorAll(".ContentItem-title a").forEach(element => {
-
             if (!element.dataset.modified) {
                 const randomString = randomChineseSubstring(element.textContent)
                 element.innerHTML = element.textContent.replace(randomString, "<span>" + randomString + "</span>")
                 element.dataset.modified = "true"
             }
         })
-
-
     }
-
-    // 每 1 秒执行一次（根据需要可调整）
     setInterval(modifyDOM, 1000);
 })();
 
 function randomChineseSubstring(str) {
-    // 过滤出纯汉字（可选）
     const chars = str.match(/[\u4e00-\u9fa5]/g);
     if (!chars || chars.length < 2) return "";
-
-    // 随机决定长度（2~4）
     const len = Math.min(Math.floor(Math.random() * 3) + 2, chars.length);
-
-    // 随机起始位置（保证不会超出范围）
     const start = Math.floor(Math.random() * (chars.length - len + 1));
-
-    // 拼接结果
     return chars.slice(start, start + len).join('')
 }
 
